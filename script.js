448c4540e3db73920e698bcae1093e78a597db6f
Sakshi Choudhary <103510144+sakshi-1140@users.noreply.github.com>
2024-06-02 17:20:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
+
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-function removeArticle(str)
-{
-    str=" "+str+" ";
-    str=str.toLowerCase();
-    str=str.replaceAll(" the "," ");
-    str=str.replaceAll(" a "," ")
-    str=str.replaceAll(" an "," ")
-    str=str.trim();
-    return str;
+const makeCustomString = (str)=>{
+    let newStr = '';
+    str.trim().toLowerCase().split(' ').forEach(word=> {
+        if (word !== 'a' && word !== 'an' && word !=='the')  newStr +=word+' ';
+    })
+    return newStr;
 }
 
-bands.sort((a,b)=>{
-    if(removeArticle(a)>removeArticle(b))
-        return 1
-    else return -1
-})
+const sortedBands = bands.sort((band1,band2)=>{
+    const slicedBand1 = makeCustomString(band1);
+    const slicedBand2 = makeCustomString(band2); 
+	if(slicedBand1 < slicedBand2) return -1;
+	else return 1;
+});
 
-//console.log(bands)
-const ul = document.getElementById('band')
-for(let t of bands)
-    {
-        const li =document.createElement('li')
-        li.textContent = t;
-        ul.append(li);
-    }
+const ul = document.getElementById('bands');
+
+sortedBands.forEach(band=>{
+	const li = document.createElement('li');
+    li.textContent = band;
+	ul.appendChild(li);	
+})
