96b442fbf596d21352c1eff65a5b6e7d6e3c8f45
Sakshi Choudhary <103510144+sakshi-1140@users.noreply.github.com>
2024-06-02 16:57:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function removeArticle(str)
+{
+    str=" "+str;
+    str=str.toLowerCase();
+    str=str.replaceAll(" the "," ");
+    str=str.replaceAll(" a "," ")
+    str=str.replaceAll(" an "," ")
+    str=str.trim();
+    return str;
+}
+
+bands.sort((a,b)=>{
+    if(removeArticle(a)>removeArticle(b))
+        return 1
+    else return -1
+})
+
+//console.log(bands)
+const ul = document.getElementById('band')
+for(let t of bands)
+    {
+        const li =document.createElement('li')
+        li.textContent = t;
+        ul.append(li);
+    }
